8abceac16a8ba54b085bf31b4ef04febb3cb13b4
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-10-18 07:32:00 +0100

    not importannt; helper script update

--- push_to_main.sh
+++ push_to_main.sh
@@ -5,15 +5,11 @@ git checkout dev
 
 # Commit any pending changes
 git add .
-<<<<<<< HEAD
-git commit -m "Update bookmark for persistent bookmark icon activate"
-=======
 git commit -m "Fix email verification error and improve error handling
 
 - Replace db.session.get() with User.query.filter_by() in verify_email function
 - Add check for non-existent user during email verification
 - Improve error messaging for invalid or expired verification links"
->>>>>>> dev
 
 # Switch to the main branch
 git checkout main
