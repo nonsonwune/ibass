2295d2a37644a9ada7eac6d03d218d65d66a3b41
Nonso Nwune <nonsonwune@Nonsos-MacBook-Pro.local>
2024-10-18 08:01:57 +0100

    Update push_to_main.sh

--- push_to_main.sh
+++ push_to_main.sh
@@ -5,11 +5,7 @@ git checkout dev
 
 # Commit any pending changes
 git add .
-git commit -m "Fix email verification error and improve error handling
-
-- Replace db.session.get() with User.query.filter_by() in verify_email function
-- Add check for non-existent user during email verification
-- Improve error messaging for invalid or expired verification links"
+git commit -m "Remove 'Number of Courses' from the Recommend cards and Changed Courses Tab to All Courses"
 
 # Switch to the main branch
 git checkout main
