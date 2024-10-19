754495c473775e61602638a9b1e5a70878e8ac34
jambgpt <nonso.chikenwune@gmail.com>
2024-10-19 04:58:42 +0100

    filter recommend

--- app.py
+++ app.py
@@ -415,10 +415,12 @@ def recommend():
         location = request.form.get("location")
         preferred_university = request.form.get("university")
         preferred_course = request.form.get("course")
+        program_type = request.form.get("program_type")
     else:  # GET request
         location = request.args.get("location")
         preferred_university = request.args.get("university")
         preferred_course = request.args.get("course")
+        program_type = request.args.get("program_type")
 
     query = db.session.query(University).outerjoin(Course)
     if location:
@@ -427,11 +429,19 @@ def recommend():
         query = query.filter(University.university_name == preferred_university)
     if preferred_course:
         query = query.filter(Course.course_name == preferred_course)
+    if program_type:
+        query = query.filter(University.program_type == program_type)
 
-    results = query.order_by(University.university_name, Course.course_name).all()
+    results = query.order_by(University.university_name).all()
+
+    # Collect universities and avoid duplicates
+    universities = {}
+    for uni in results:
+        if uni.id not in universities:
+            universities[uni.id] = uni
 
     recommendations = []
-    for uni in results:
+    for uni in universities.values():
         uni_data = {
             "id": uni.id,
             "university_name": uni.university_name,
@@ -451,12 +461,24 @@ def recommend():
         }
         recommendations.append(uni_data)
 
+    # Get the list of available program types for the filter
+    program_types_query = db.session.query(University.program_type).distinct()
+    program_types = [pt[0] for pt in program_types_query.all()]
+
+    # Get user's bookmarks if authenticated
+    user_bookmarks = set()
+    if current_user.is_authenticated:
+        user_bookmarks = set(
+            bookmark.university_id for bookmark in current_user.bookmarks
+        )
+
     return render_template(
         "recommend.html",
         recommendations=recommendations,
         location=location,
         university=preferred_university,
         course=preferred_course,
+        user_bookmarks=user_bookmarks,
     )
 
 
